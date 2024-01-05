15416d81ca38e142a5617861f263713ca2f7ea76
mehdi <mehdi@DESKTOP-VARGV30>
2024-01-05 13:45:28 +0330

    Add project files.

---
+++ WebApplication2/controllers1/homecontroller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.controllers1
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class Homecontroller : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddTest([FromForm] Test test)
+        {
+            string data = $"""
+                       <form>
+                       <input type="text" name="FirstName" value="{test.FirstName}"
+                       <input type="text" name="{nameof(test.LastName)}" value="{test.LastName}"
+                       </form>
+                       """;
+            return Ok(data);
+        }
+    }
+    public record Test 
+    {
+        public required string FirstName {  get; set; }
+        public required string LastName { get; set; }
+    }
+}
+
